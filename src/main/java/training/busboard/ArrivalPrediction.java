ecaf5c325c4094cd1840b94d404578d5ec33cd56
ZOO\AreMel <arend.mellendijk@softwire.com>
2021-07-05 15:17:22 +0100

    Added deserialization code

---
+++ src/main/java/training/busboard/ArrivalPrediction.java
@@ -0,0 +1,10 @@
+package training.busboard;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ArrivalPrediction {
+    public String vehicleId;
+    public String expectedArrival;
+
+}
--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -1,7 +1,24 @@
 package training.busboard;
 
+import org.glassfish.jersey.jackson.JacksonFeature;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import java.lang.reflect.Array;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String args[]) {
-        // Your code here!
+        Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
+
+        List<ArrivalPrediction> response = client.target("https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals")
+            .request(MediaType.APPLICATION_JSON)
+            .get(new GenericType<List<ArrivalPrediction>>() {});
+
+        for(ArrivalPrediction bus : response)
+            System.out.println(bus.expectedArrival);
     }
 }	
